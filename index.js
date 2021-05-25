af9fb4f06fc503df1a455fd4c50996f418b8eaf6
devevignesh <vigneshe1992@gmail.com>
2021-05-25 23:16:32 +0530

    asana integration

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const asana = require("asana");
+
+async function addComment(asanaPAT, taskId, taskComment) {
+    try {
+        const asanaClient = asana.Client.create({
+            defaultHeaders: { "asana-enable": "new_sections,string_ids" },
+            logAsanaChangeWarnings: false
+        }).useAccessToken(asanaPAT);
+
+        await client.tasks.addComment(taskId, {
+            text: taskComment
+        });
+        core.info("Added the pull request link to the Asana task.");
+    } catch (err) {
+        core.setFailed(err.value);
+    }
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const asanaPat = core.getInput("asana-pat");
+    const taskComment = core.getInput("task-comment");
+    const triggerPharse = core.getInput("trigger-phrase");
+    const pullRequests = github.context.payload.pull_request;
+    const formattedLink = new RegExp(
+        `${triggerPharse} *\\[(.*?)\\]\\(https:\\/\\/app.asana.com\\/(\\d+)\\/(?<project>\\d+)\\/(?<task>\\d+).*?\\)`,
+        "g"
+    );
+    const parseAsanaURL = REGEX.exec(PULL_REQUEST.body);
+
+    if (parseAsanaURL !== null && taskComment) {
+        const taskId = parseAsanaURL.groups.task;
+        if (taskId) {
+            addComment(asanaPat, taskId, taskComment);
+        }
+    } else {
+        core.info(`Invalid Asana task URL ${triggerPharse}`);
+    }
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
